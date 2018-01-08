97e7ed82983c9150fe02bea34d31e70ff72d1845
Mike Conway <mike.conway@nih.gov>
2018-01-08 16:53:09 -0500

    #1 align with new jargon

--- irodsext-data-profiler/src/test/java/org/irodsext/dataprofiler/IrodsextDataProfilerServiceTest.java
+++ irodsext-data-profiler/src/test/java/org/irodsext/dataprofiler/IrodsextDataProfilerServiceTest.java
@@ -3,13 +3,17 @@
  */
 package org.irodsext.dataprofiler;
 
+import java.io.File;
 import java.util.Properties;
 
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.connection.JargonProperties;
 import org.irods.jargon.core.connection.SettableJargonProperties;
+import org.irods.jargon.core.pub.DataTransferOperations;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
 import org.irods.jargon.core.pub.IRODSFileSystem;
+import org.irods.jargon.core.pub.io.IRODSFile;
+import org.irods.jargon.core.pub.io.IRODSFileFactory;
 import org.irods.jargon.extensions.dataprofiler.DataProfile;
 import org.irods.jargon.extensions.dataprofiler.DataProfilerService;
 import org.irods.jargon.extensions.dataprofiler.DataProfilerSettings;
@@ -17,6 +21,7 @@ import org.irods.jargon.extensions.datatyper.DataTypeResolutionService;
 import org.irods.jargon.extensions.datatyper.DataTyperSettings;
 import org.irods.jargon.testutils.IRODSTestSetupUtilities;
 import org.irods.jargon.testutils.TestingPropertiesHelper;
+import org.irods.jargon.testutils.filemanip.FileGenerator;
 import org.irods.jargon.testutils.filemanip.ScratchFileUtils;
 import org.irodsext.datatyper.IrodsextDataTypeResolutionService;
 import org.junit.After;
@@ -102,6 +107,62 @@ public class IrodsextDataProfilerServiceTest {
 		@SuppressWarnings("rawtypes")
 		DataProfile dataProfile = dataProfilerService.retrieveDataProfile(targetIrodsCollection);
 		Assert.assertNotNull("null data profile returned", dataProfile);
+		Assert.assertFalse("no acls", dataProfile.getAcls().isEmpty());
+		Assert.assertFalse("no child name", dataProfile.getChildName().isEmpty());
+		Assert.assertFalse("no parent path", dataProfile.getParentPath().isEmpty());
+		Assert.assertFalse("no path components", dataProfile.getPathComponents().isEmpty());
+		Assert.assertNotNull("no domain object", dataProfile.getDomainObject());
+
+	}
+
+	@Test
+	public void testBasicDataProfileWithTextFileAndFileExtensionTyping() throws Exception {
+		IRODSAccount irodsAccount = testingPropertiesHelper.buildIRODSAccountFromTestProperties(testingProperties);
+		IRODSAccessObjectFactory accessObjectFactory = irodsFileSystem.getIRODSAccessObjectFactory();
+
+		String testFileName = "testBasicDataProfileWithTextFileAndFileExtensionTyping.txt";
+		String absPath = scratchFileUtils.createAndReturnAbsoluteScratchPath(IRODS_TEST_SUBDIR_PATH);
+		String localFileName = FileGenerator.generateFileOfFixedLengthGivenName(absPath, testFileName, 1);
+
+		File localFile = new File(localFileName);
+
+		// now put the file
+		String targetIrodsFile = testingPropertiesHelper
+				.buildIRODSCollectionAbsolutePathFromTestProperties(testingProperties, IRODS_TEST_SUBDIR_PATH);
+
+		IRODSFileFactory irodsFileFactory = accessObjectFactory.getIRODSFileFactory(irodsAccount);
+		IRODSFile destFile = irodsFileFactory.instanceIRODSFile(targetIrodsFile);
+		DataTransferOperations dataTransferOperations = accessObjectFactory.getDataTransferOperations(irodsAccount);
+		dataTransferOperations.putOperation(localFile, destFile, null, null);
+		String dataName = destFile.getAbsolutePath() + "/" + testFileName;
+
+		DataProfilerSettings dataProfilerSettings = new DataProfilerSettings();
+		dataProfilerSettings.setDetectMimeAndInfoType(true);
+		dataProfilerSettings.setRetrieveAcls(true);
+		dataProfilerSettings.setRetrieveMetadata(true);
+		dataProfilerSettings.setRetrieveReplicas(false);
+		dataProfilerSettings.setRetrieveShared(false);
+		dataProfilerSettings.setRetrieveStarred(false);
+		dataProfilerSettings.setRetrieveTickets(false);
+		DataTyperSettings dataTyperSettings = new DataTyperSettings();
+		dataTyperSettings.setDetailedDetermination(false);
+		dataTyperSettings.setPersistDataTypes(false);
+		DataTypeResolutionService dataTyperService = new IrodsextDataTypeResolutionService(accessObjectFactory,
+				irodsAccount, dataTyperSettings);
+
+		DataProfilerService dataProfilerService = new IrodsextDataProfilerService(dataProfilerSettings,
+				accessObjectFactory, irodsAccount);
+		dataProfilerService.setDataTypeResolutionService(dataTyperService);
+		@SuppressWarnings("rawtypes")
+		DataProfile dataProfile = dataProfilerService.retrieveDataProfile(dataName);
+		Assert.assertNotNull("null data profile returned", dataProfile);
+		Assert.assertFalse("no acls", dataProfile.getAcls().isEmpty());
+		Assert.assertFalse("no child name", dataProfile.getChildName().isEmpty());
+		Assert.assertFalse("no parent path", dataProfile.getParentPath().isEmpty());
+		Assert.assertFalse("no path components", dataProfile.getPathComponents().isEmpty());
+		Assert.assertNotNull("no domain object", dataProfile.getDomainObject());
+		Assert.assertTrue("is not a file", dataProfile.isFile());
+		Assert.assertNotNull("null data type", dataProfile.getDataType());
 
 	}
 
