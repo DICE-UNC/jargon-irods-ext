774ad30b521c88582e5917eb2384895566b8b25c
Mike Conway <mike.conway@nih.gov>
2018-08-08 09:29:35 -0400

    #3 test properties

--- irodsext-metadata-template/src/test/java/org/irodsext/mdtemplate/TemplateTest.java
+++ irodsext-metadata-template/src/test/java/org/irodsext/mdtemplate/TemplateTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.threeten.bp.OffsetDateTime;
@@ -24,6 +25,7 @@ import junit.framework.TestCase;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
+@TestPropertySource("classpath:test.metadatatemplate.properties")
 public class TemplateTest extends TestCase{
 
 	/*@Autowired
