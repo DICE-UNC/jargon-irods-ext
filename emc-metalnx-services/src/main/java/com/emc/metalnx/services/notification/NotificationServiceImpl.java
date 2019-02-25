43194d7dc85877f6ac165aa3ed2172ef1332329a
Mike Conway <mike.conway@nih.gov>
2019-02-25 09:58:38 -0500

    #7 add error throw and logging

--- emc-metalnx-services/src/main/java/com/emc/metalnx/services/notification/NotificationServiceImpl.java
+++ emc-metalnx-services/src/main/java/com/emc/metalnx/services/notification/NotificationServiceImpl.java
@@ -1,7 +1,5 @@
 package com.emc.metalnx.services.notification;
 
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,23 +8,30 @@ import java.util.Map;
 import org.datacommons.api.NotificationsApi;
 import org.datacommons.client.ApiException;
 import org.datacommons.model.Notification;
+import org.irods.jargon.core.exception.JargonRuntimeException;
 import org.irods.jargon.extensions.notification.NotificationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 @Service
-public class NotificationServiceImpl implements NotificationService{
+public class NotificationServiceImpl implements NotificationService {
 
-	//@Autowired
+	private static final Logger logger = LoggerFactory.getLogger(NotificationServiceImpl.class);
+
+	// @Autowired
 	NotificationsApi apiInstance = new NotificationsApi();
-	
+
 	@Override
 	public List<Notification> getAllNotification(String userId) {
 		// TODO Auto-generated method stub
 		List<Notification> result = new ArrayList<>();
-		 try {
+		try {
 			result = apiInstance.getNotification(userId);
 		} catch (ApiException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error("unable to retrieve notifications", e);
+			throw new JargonRuntimeException("error retrieving notifications", e); // TODO: consider what exception
+																					// hierarchy needs to look like
 		}
 		return result;
 	}
@@ -46,26 +51,26 @@ public class NotificationServiceImpl implements NotificationService{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 
+
 		return result;
 	}
 
 	@Override
 	public void deleteNotifications(List<String> uuids) {
 		// TODO Auto-generated method stub
-		
-		
+
 	}
 
 	@Override
 	public void markToSeen(List<String> uuids) {
-		/* NOTE - Conversion should be kept in the services-- this should be the list of String/UUIDs. 
-			and the services should convert to any supported or desired format.*/
-		/*try {
-			apiInstance.markSeen(uuids);
-		} catch (ApiException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		/*
+		 * NOTE - Conversion should be kept in the services-- this should be the list of
+		 * String/UUIDs. and the services should convert to any supported or desired
+		 * format.
+		 */
+		/*
+		 * try { apiInstance.markSeen(uuids); } catch (ApiException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
 	}
 }
