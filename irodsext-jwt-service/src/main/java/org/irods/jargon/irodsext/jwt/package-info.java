e45103ec821e414018aa5b2be89c55ff2bdc14d1
Mike Conway <mike.conway@nih.gov>
2019-08-07 09:59:01 -0400

    #10 migrate jwt code out to stand alone lib to avoid circular dependencies

--- irodsext-jwt-service/src/main/java/org/irods/jargon/irodsext/jwt/package-info.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Utils and services to support use of JWT tokens in Jargon extensions and
- * microservices
- * 
- * @author Mike Conway - NIEHS
- *
- */
-package org.irods.jargon.irodsext.jwt;
