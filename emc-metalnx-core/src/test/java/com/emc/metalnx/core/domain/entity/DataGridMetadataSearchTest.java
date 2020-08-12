414f4fd80ccc5acbff1277ce927c9803da5830be
Mike Conway <mike.conway@nih.gov>
2020-08-12 12:29:36 -0400

    #17 fix for utf8 in metadata search

---
+++ emc-metalnx-core/src/test/java/com/emc/metalnx/core/domain/entity/DataGridMetadataSearchTest.java
@@ -0,0 +1,25 @@
+package com.emc.metalnx.core.domain.entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.emc.metalnx.core.domain.entity.enums.DataGridSearchOperatorEnum;
+
+public class DataGridMetadataSearchTest {
+
+	@Test
+	public void testGetSpecQueryAsString() {
+		String testAttrib = "Sîne klâwen durh die wolken sint geslagen";
+		String testValue = "На берегу пустынных волн";
+		String testUnit = "Ég get etið gler án þess að meiða mig";
+		DataGridMetadataSearch dataGridMetadataSearch = new DataGridMetadataSearch(testAttrib, testValue, testUnit,
+				DataGridSearchOperatorEnum.EQUAL);
+		String queryString = dataGridMetadataSearch.getSpecQueryAsString();
+		Assert.assertNotNull(queryString);
+		Assert.assertEquals(
+				" SELECT map.object_id AS map_object_id  FROM r_objt_metamap map  JOIN (      SELECT m.meta_id, m.meta_attr_name, m.meta_attr_value     FROM r_meta_main m  WHERE   LOWER( m.meta_attr_name ) = LOWER( 'Sîne klâwen durh die wolken sint geslagen' )  AND   LOWER( m.meta_attr_value ) = LOWER(  'На берегу пустынных волн'  )  AND  LOWER( m.meta_attr_unit ) = LOWER(  'Ég get etið gler án þess að meiða mig'  )   )  AS metadata ON (metadata.meta_id = map.meta_id)  GROUP BY map.object_id  HAVING COUNT(map.meta_id) > 0 ",
+				queryString);
+
+	}
+
+}
