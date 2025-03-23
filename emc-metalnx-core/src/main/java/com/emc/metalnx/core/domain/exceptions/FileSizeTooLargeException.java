8f12fbbece2292efbf4c3c283ba0ee39289cf3dd
Kory Draughn <korydraughn@ymail.com>
2025-03-23 09:30:06 -0400

    [irods-contrib/metalnx-web#392] Include size in file size exception.

--- emc-metalnx-core/src/main/java/com/emc/metalnx/core/domain/exceptions/FileSizeTooLargeException.java
+++ emc-metalnx-core/src/main/java/com/emc/metalnx/core/domain/exceptions/FileSizeTooLargeException.java
@@ -1,5 +1,7 @@
 package com.emc.metalnx.core.domain.exceptions;
 
+import java.util.Optional;
+
 /**
  * A file or bundle size is too large for the given operation (e.g. download)
  * 
@@ -9,12 +11,23 @@ package com.emc.metalnx.core.domain.exceptions;
 public class FileSizeTooLargeException extends DataGridException {
 
 	private static final long serialVersionUID = -7006504069756007074L;
+	
+	private Optional<Long> fileSize = Optional.empty();
 
 	public FileSizeTooLargeException() {
 	}
 
 	public FileSizeTooLargeException(String msg) {
 		super(msg);
+	}
+
+	public FileSizeTooLargeException(String msg, long fileSize) {
+		super(msg);
+		this.fileSize = Optional.of(fileSize);
+	}
+	
+	public Optional<Long> getFileSize() {
+		return fileSize;
 	}
 
 }
